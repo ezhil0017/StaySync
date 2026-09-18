388db71f6c6f2356495d7715a51744fc13236e88
ezhil0017 <ezhil0017@gmail.com>
2026-09-18 23:22:52 +0530

    feat: added Global Exception handler missed the annotation to handle exceptions

--- src/main/java/com/example/StaySync/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/StaySync/exception/GlobalExceptionHandler.java
@@ -4,15 +4,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.OffsetDateTime;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleResourceNotFound(ResourceNotFoundException exception){
         ErrorResponse errorResponse=new ErrorResponse(HttpStatus.NOT_FOUND.value(),exception.getMessage(), OffsetDateTime.now());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
+
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public  ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException exception){
