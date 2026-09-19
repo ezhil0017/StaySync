e5a2c3495cd814bee44df559692a3d72baa0c558
ezhil0017 <ezhil0017@gmail.com>
2026-09-19 23:30:26 +0530

    feat: added Query for get room availability status

---
+++ src/main/java/com/example/StaySync/repository/BookingRepository.java
@@ -0,0 +1,29 @@
+package com.example.StaySync.repository;
+
+import com.example.StaySync.entity.Booking;
+import com.example.StaySync.enums.BookingStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.OffsetDateTime;
+
+public interface BookingRepository extends JpaRepository<Booking,Long> {
+
+    @Query("""
+            select count(b) > 0
+            from Booking b
+            where b.room.roomId = :roomId
+            and b.status <> :cencelledStatus
+            and b.checkIn < :checkOut
+            and b.checkOut >:checkIn
+            """)
+
+    boolean existsOverlappingBooking(
+            @Param("roomId") Long roomId,
+            @Param("checkIn") OffsetDateTime checkIn,
+            @Param("checkOut") OffsetDateTime checkOut,
+            @Param("cancelledStatus")BookingStatus cancelledStatus
+
+            );
+}
